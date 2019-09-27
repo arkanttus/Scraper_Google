59716fcb6f3fb68ca08b4b455db879457756f9c9
Italo Oliveira <38302009+arkanttus@users.noreply.github.com>
2019-09-27 15:09:32 -0500

    Code update

--- bot_google.py
+++ bot_google.py
@@ -5,10 +5,9 @@ import os
 import sys
 
 def search(query):
-    #query = urllib.parse.quote_plus(query)
     url = "https://www.google.com/search?q=" + query
-    print(url)
     
+    #header para simular o acesso vindo de um navegador
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     response=requests.get(url, headers=headers)
 
@@ -16,24 +15,19 @@ def search(query):
     conts = soup.find_all(class_='rc')
     #conts = soup.select('.rc > .r')
     
-    #print(conts)
-    #print(conts.find('a'))
-    
     for cont in conts:
         r = cont.find(class_='r')
-        #print(r)
         textLink = r.find('a')
         #textLink = cont.find('a')
-        print(textLink)
         texto, link = textLink.find(class_='ellip'), textLink['href']
         print(texto.text)
         print(link)
         print('-----')
     
 
-#search('carro')
-query = sys.argv[1]
-print(query)
+##### MAIN ######
+
+query = sys.argv[1] #capturar parametro do terminal
 search(query)
 
 
